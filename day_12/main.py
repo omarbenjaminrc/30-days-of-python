598e4e51eb9111ac65a714def0eccdd7ad636593
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2022-10-09 19:08:46 -0300

    math

--- day_12/main.py
+++ day_12/main.py
@@ -1,5 +1,5 @@
 import mi_modulo
-import  math, datetime, os,sys, random, statistics, collections, json,re
+import  math, datetime, os,sys, random, statistics, collections, json,re,string
 
 # nombre = mi_modulo.generar_nombre_completo('Omar','Ramirez')
 # print(nombre)
@@ -15,4 +15,18 @@ import  math, datetime, os,sys, random, statistics, collections, json,re
 # sys
 # print(sys.maxsize)
 
+# statistics
 
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(statistics.mean(ages))       # ~22.9
+print(statistics.median(ages))     # 23
+print(statistics.mode(ages))       # 20
+print(statistics.stdev(ages))      # ~2.3
+
+
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(2))      # 1.4142135623730951, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
