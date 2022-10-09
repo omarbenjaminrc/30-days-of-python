10db12daaee994c5e67b9f3d29e4508d743d357c
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2022-10-09 17:10:57 -0300

    jugando con modulos

---
+++ day_12/main.py
@@ -0,0 +1,13 @@
+import mi_modulo
+import  math, datetime, os,sys, random, statistics, collections, json,re
+
+nombre = mi_modulo.generar_nombre_completo('Omar','Ramirez')
+print(nombre)
+
+# os
+
+# os.chdir('day_1')
+# # os.mkdir('carpeta_001')
+# direccion = os.getcwd()
+# print(direccion)
+# os.rmdir('carpeta_001')
---
+++ day_12/mi_modulo.py
@@ -0,0 +1,2 @@
+def generar_nombre_completo(primer_nombre,apellido):
+    return f' {primer_nombre} {apellido} '
