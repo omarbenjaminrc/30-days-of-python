30b25db31a0ab6a7a025b1fe8de4aba8f17d3dca
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2022-10-10 12:36:18 -0300

    añadi funciones: rgb_color_gen y hexa_colors

--- day_12/level_1.py
+++ day_12/level_1.py
@@ -1,8 +1,5 @@
 import random,string
 
-def generar_nombre_completo(primer_nombre,apellido):
-    return f' {primer_nombre} {apellido} '
-
 def random_user_id():
     todo = string.ascii_letters + string.digits
     user_id = ''
@@ -20,3 +17,18 @@ def random_user_id_by_user(ides,caracteres):
         lista.append(user_id)
         
     return lista
+
+def rgb_color_gen():
+    color = []
+    for i in range(3):
+        color.append(random.randint(0,255))
+    return f'rgb({color})'
+
+def hexa_colors():
+    lista = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+    hexadecimal = ''
+    for i in range(6):
+        hexadecimal += random.choice(lista)
+    return hexadecimal
+
+
